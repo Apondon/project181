fca0f08903a9cb97e7466967b21f0a0001dcc41f
apondon <yyq930528@163.com>
2020-04-07 09:15:41 +0800

    commit 0407

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,125 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-container>
+      <el-header class='hdr'>
+        <el-row :gutter="20">
+          <el-col :span="8"><div class="logo">小莱书城</div></el-col>
+          <el-col :span="16">
+            <div class="breadcrumb">
+              <el-breadcrumb separator="|">
+                <el-breadcrumb-item><span @click="loginVisible = true">登录</span></el-breadcrumb-item>
+                <el-breadcrumb-item><span @click="registVisible = true">注册</span></el-breadcrumb-item>
+                <el-breadcrumb-item><span @click="aboutVisible = true">关于</span></el-breadcrumb-item>
+              </el-breadcrumb>
+            </div>
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-main class="ctt">
+        <div class="inner">
+          <router-view/>
+        </div>
+      </el-main>
+      <el-footer class="ftr">Footer</el-footer>
+    </el-container>
+    <!-- 登录 -->
+    <el-dialog :visible.sync="loginVisible">
+      <el-form :model="loginForm">
+        <el-form-item label="账号" :label-width="formLabelWidth">
+          <el-input v-model="loginForm.account" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" :label-width="formLabelWidth">
+          <el-input v-model="loginForm.password" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        
+        <el-button type="primary" @click="loginVisible = false">登 录</el-button>
+      </div>
+    </el-dialog>
+    <!-- 注册 -->
+    <el-dialog :visible.sync="registVisible">
+      <el-form :model="registForm">
+        <el-form-item label="账号" :label-width="formLabelWidth">
+          <el-input v-model="registForm.account" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" :label-width="formLabelWidth">
+          <el-input v-model="registForm.password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" :label-width="formLabelWidth">
+          <el-input v-model="registForm.cfmpassword" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="registVisible = false">提 交</el-button>
+      </div>
+    </el-dialog>
+    <!-- 关于 -->
+    <el-dialog :visible.sync="aboutVisible" >
+      <span>需要设置visible属性，它接收Boolean，当为true时显示 Dialog。Dialog 分为两个部分：body和footer，footer需要具名为footer的slot。title属性用于定义标题，它是可选的，默认值为空。最后，本例还展示了before-close的用法。</span>
+    </el-dialog>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data(){
+    return{
+      // 登录
+      loginVisible:false,
+      loginForm:{
+        account:'',
+        password:''
+      },
+      // 注册
+      registVisible : false,
+      registForm:{
+        account:'',
+        password:'',
+        cfmpassword:''
+      },
+      // 关于
+      aboutVisible:false,
+      // label的宽度
+      formLabelWidth: '120px'
     }
+  }
+}
+</script>
+<style lang="scss">
+html,body{
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+#app{
+  height: 100%;
+  section{
+    height: 100%;
+  }
+  .hdr{
+    background: navy;
+    .logo{
+      line-height: 60px;
+      color: #fff;
+      font-size: 40px;
+    }
+    .breadcrumb{
+      height: 60px;
+      .el-breadcrumb{
+        line-height: 60px;
+        box-sizing: border-box;
+        padding-left: 70%;
+        span{
+          color:#fff;
+        }
+      }
+    }
+  }
+  .ctt{
+    background: #f4f4f4;
+  }
+  .ftr{
+    background: gainsboro;
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,191 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <el-row :gutter="20">
+        <el-col :span="4">
+          <div class="bkgw">
+            <h4>全部书籍</h4>
+            <div v-for="(item) in list" :key='item.id' class="bookmenu">
+              <h5>{{item.title}}</h5>
+              <p v-for="(items) in item.books" :key='items.id'>
+                {{items.name}}
+              </p>
+            </div>
+          </div>
+        </el-col>
+        <el-col :span="20">
+          <div class="banner"></div>
+          <!-- 卡片 -->
+          <el-row :gutter="10">
+            <el-col :span="12" v-for="(item) in cards" :key="item.id">
+              <el-card>
+                <el-row :gutter="0">
+                  <el-col :span="8">
+                    <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" height="100%" class="image">
+                  </el-col>
+                  <el-col :span="16">
+                    <span class="card_title">{{item.title}}</span>
+                    <p class="mtp10">{{item.text}}</p>
+                    <p>{{item.text}}</p>
+                    <el-button type="primary" class="buyBtn">立即购买</el-button>
+                  </el-col>
+                </el-row>
+              </el-card>
+            </el-col>
+          </el-row>
+          <!-- 卡片结束 -->
+        </el-col>
+      </el-row>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      list:[
+        {
+          id:1,
+          title:'移动互联网开发专业书籍',
+          books:[
+            {
+              id:'pro1',
+              name:'web标准网页设计之道'
+            },
+            {
+              id:'pro2',
+              name:'photoshop商业网站设计'
+            },
+            {
+              id:'pro3',
+              name:'c#编程基础'
+            },
+            {
+              id:'pro4',
+              name:'sql server数据库基础'
+            },
+            {
+              id:'pro5',
+              name:'java语言基础'
+            },
+            {
+              id:'pro6',
+              name:'java面向对象编程'
+            },
+            {
+              id:'pro7',
+              name:'java语言高级特性'
+            },
+          ]
+        },
+        {
+          id:2,
+          title:'移动互联网应用专业书籍',
+          books:[
+            {
+              id:'apl1',
+              name:'计算机基础应用'
+            },
+            {
+              id:'apl2',
+              name:'linux系统管理'
+            },
+            {
+              id:'apl3',
+              name:'企业网络互联技术'
+            },
+            {
+              id:'apl4',
+              name:'windows网络服务管理'
+            },
+            {
+              id:'apl5',
+              name:'windows系统管理'
+            },
+            {
+              id:'apl6',
+              name:'windows活动目录管理'
+            },
+            {
+              id:'apl7',
+              name:'计算机网络技术'
+            },
+          ]
+        }
+      ],
+      cards:[
+        {
+          id:1,
+          img:'',
+          title:'移动互联网开发全套书籍',
+          text:'移动互联网开发配套的书籍有好几本然后买不买无所谓就是凑数的'
+        },
+        {
+          id:2,
+          img:'',
+          title:'移动互联网应用全套书籍',
+          text:'移动互联网开发配套的书籍有好几本然后买不买无所谓就是凑数的'
+        },
+        {
+          id:3,
+          img:'',
+          title:'大数据全套书籍',
+          text:'移动互联网开发配套的书籍有好几本然后买不买无所谓就是凑数的'
+        },
+        {
+          id:4,
+          img:'',
+          title:'人工智能全套书籍',
+          text:'移动互联网开发配套的书籍有好几本然后买不买无所谓就是凑数的'
+        },
+      ]
+    }
   }
 }
 </script>
+<style lang="scss" scoped>
+.home{
+  // padding: 20px 0;
+  h4,h5,p{
+    margin: 0;
+    text-indent: 10px;
+  }
+  h4{
+    height: 40px;
+    line-height: 40px;
+    background: navy;
+    color: #fff;
+  }
+  h5{
+    height: 55px;
+    line-height: 55px;
+    font-weight: 100;
+    font-size: 20px;
+  }
+  p{
+    height: 30px;
+    line-height: 30px;
+  }
+  .bookmenu{
+    margin-bottom: 3px;
+    background: #fff;
+  }
+  
+  .banner{
+    height:300px;
+    background: navy;
+    margin-bottom: 20px;
+  }
+  .card_title{
+    padding-left: 10px;;
+    font-size: 22px;
+    font-weight: 600;
+  }
+  .mtp10{
+    margin-top: 10px;
+  }
+  .buyBtn{
+    margin: 20px 0 0 10px;
+  }
+}
+</style>
